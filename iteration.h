5bc5d6df335c773b8f2f0dc85b5c3e1868948e87
Lorenz <lorenz.hufnagel@fau.de>
2014-05-27 17:09:38 +0200

    Iteration header

---
+++ iteration.h
@@ -0,0 +1,9 @@
+#include <math.h>
+
+double euclNorm(vector<double>* u);
+
+void vectorScalar(vector<double>* u,double scalar);
+
+double vectorVector(vector<double>* u,vector<double>* v);
+
+void matrixVector(vector<map<int,double> >* mat,vector<double>* vec,vector<double>* ret);
