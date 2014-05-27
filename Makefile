527b771b40516b74703756b93550f10a0cdd74c5
HugoMichi <mu23jyhu@i10cip9.i10.informatik.uni-erlangen.de>
2014-05-27 15:27:16 +0200

    matrix zusammenbauen und header neu

--- Makefile
+++ Makefile
@@ -1,16 +1,31 @@
 CXX = g++
-CXXFLAGS ?= -ansi -Winline -Wshadow -Wall -O3
-BIN = readIn
-SOURCES = readin.cpp
-HEADERS = 
-LIBS = 
+CXXFLAGS ?= -ansi  -Wshadow -Wall -O3 #-fopenmp -Winline
+#-ggdb
+SOURCES = readin.cpp matrixes.cpp
+OBJECTS = readin.o matrixes.o
+HEADERS = readin.h matrixes.h
+LIBS = -lm
 
 .PHONY: all clean
 
-all: ${BIN}
+all: readin.o matrixes.o test
 
-${BIN}: ${SOURCES}
-	${CXX} ${CXXFLAGS} -o $@ $< 
+# generic compilation rule
+readin.o : readin.cpp
+	${CXX} ${CXXFLAGS} -c $<
+
+matrixes.o : matrixes.cpp
+	${CXX} ${CXXFLAGS} -c $<
+
+#%.o : %.cpp
+#	${CXX} ${CXXFLAGS} -c $<
+
+#how to link
+test: ${OBJECTS}
+	${CXX} ${CXXFLAGS} -o $@ ${OBJECTS} ${LIBS}
+
+#readin: readin.o
+#	${CXX} ${CXXFLAGS} -o $@ $@.o ${LIBS}
 
 clean:
-	rm -f *.o *~ *.swp ${BIN}
+	rm -f *.o *~ *.txt  *.dat
--- matrixes.cpp
+++ matrixes.cpp
@@ -1,30 +1,126 @@
-#include <map>
+
+/*#include <map>
 #include <vector>
+#include "myColsamm/Source/Colsamm.h"
+#include "readin.h"*/
+#include "matrixes.h"
 
-using namespace ::_COLSAMM_;
-using namespace std;
+//using namespace ::_COLSAMM_;
+//using namespace std;
 
-void add_global_matrix(vector<map< int,double> > glob_matrix,Face element,vector< vector< double > > my_local_matrix
-){
-    for (int j=0;j<3;++j){
-        for (int i=0;i<3;++i){
-            glob_matrix[element.vertex[j]][element.vertex[i]]+=my_local_matrix[j][i];
-        }
-    }
+
+
+
+void add_to_global_matrix(vector<map< int,double> >* glob_matrix,Face element,vector< vector<double> >* my_local_matrix){
+
+   //glob_matrix->at(1)=1.0;
+
+
+
+            glob_matrix->at(element.vertex0->index)[element.vertex0->index]+=my_local_matrix->at(0).at(0);
+            glob_matrix->at(element.vertex1->index)[element.vertex0->index]+=my_local_matrix->at(1).at(0);
+            glob_matrix->at(element.vertex2->index)[element.vertex0->index]+=my_local_matrix->at(2).at(0);
+
+            glob_matrix->at(element.vertex0->index)[element.vertex1->index]+=my_local_matrix->at(0).at(1);
+            glob_matrix->at(element.vertex1->index)[element.vertex1->index]+=my_local_matrix->at(1).at(1);
+            glob_matrix->at(element.vertex2->index)[element.vertex1->index]+=my_local_matrix->at(2).at(1);
+
+            glob_matrix->at(element.vertex0->index)[element.vertex2->index]+=my_local_matrix->at(0).at(2);
+            glob_matrix->at(element.vertex1->index)[element.vertex2->index]+=my_local_matrix->at(1).at(2);
+            glob_matrix->at(element.vertex2->index)[element.vertex2->index]+=my_local_matrix->at(2).at(2);
+
+
+
 
     //vector<map< int,double> > glob_matrix = new vector<map< int,double > >(n);
     /*for (int i=0;i<n;++n){
-        for(int j=0;j<vertices.num_neighbours;++j){
-            glob_matrix[i][vertices.neighbours[j]]= vertices[]
+for(int j=0;j<vertices.num_neighbours;++j){
+glob_matrix[i][vertices.neighbours[j]]= vertices[]
 
 
-        }
-    }*/
+}
+}*/
 }
 
 vector<map< int,double> > create_global_matrix(int n){
-    vector<map< int,double> > glob_matrix=new vector<map< int,double> >(n);
-    for (int i=0;i<n;++n){
-        glob_matrix[i]=new map< int,double> ();
+    vector<map< int,double> > glob_matrix(n);//=new vector<map< int,double> >(n);
+    for (int i=0;i<n;++i){
+        glob_matrix[i];//=new map< int,double> ();
     }
+    return glob_matrix;
+}
+
+
+vector<map< int,double> > get_build_matrix( vector<Point>* points, vector<Face>* faces){
+    cout<<"test"<<endl;
+    ELEMENTS::Triangle my_element;
+    vector<map< int,double> > glob_matrix = create_global_matrix(points->size());
+    std::vector< std::vector< double > > my_local_matrix;
+    std::vector<double> corners(6, 0.0);
+
+
+    for(int i=0; i<faces->size();++i){
+        // array corners contains the x- and y-coordinates of the
+        // triangle corners in the order x0, y0, x1, y1, x2, y2
+        corners[0] = faces->at(i).vertex0->x;
+        corners[1] = faces->at(i).vertex0->y;
+        corners[2] = faces->at(i).vertex1->x;
+        corners[3] = faces->at(i).vertex1->y;
+        corners[4] = faces->at(i).vertex2->x;
+        corners[5] = faces->at(i).vertex2->y;
+        cout<<i<<endl;
+        // pass the corners to the finite element
+        my_element(corners);
+        cout<<i<<endl;
+        my_local_matrix = my_element.integrate(grad(v_()) * grad(w_()));
+        cout<<i<<endl;
+        add_to_global_matrix(&glob_matrix,faces->at(i),&my_local_matrix);
+    }
+
+    return glob_matrix;
+}
+
+bool save_global_matrix(vector<map< int,double> >* glob_matrix, const char* name){
+    ofstream file;
+    file.open(name, ios::out);
+      if(!(file.is_open())){
+          printf(" konnte nicht gespeichert werden\n");
+          exit(1);
+      }
+      for(int j = 0;j<glob_matrix->size();++j){
+          for(int i = 0; i < glob_matrix->size(); i++){
+              if(glob_matrix->at(j).count(i)==1){
+                  file <<glob_matrix->at(j)[i]  << ' ';
+              }else{
+                   file <<0.0  << ' ';
+              }
+          }
+          file << endl;
+       }
+
+          file.close();
+}
+
+
+
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+   vector<Point> points;
+   vector<Face> faces;
+
+   bool success =readFromFile("./inputs/unit_circle.txt",&points,&faces);
+  cout<<success<<endl;
+   cout<<"sizefaces:"<<faces.size()<<endl;
+   //cout << faces.front().vertex0->x;
+   vector<map< int,double> > glob_matrix=get_build_matrix( &points, &faces);
+
+
+
+
+   return 0;
 }
